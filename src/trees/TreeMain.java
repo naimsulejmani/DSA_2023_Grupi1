ce3d6cbd41b786db0894f4f208e2e571064a2e31
Naim Sulejmani <naim.sulejmani@radix.com>
2023-05-03 17:23:38 +0200

    Binary search tree started

--- src/trees/TreeMain.java
+++ src/trees/TreeMain.java
@@ -1,4 +1,26 @@
 package trees;
 
+import trees.bst.BinarySearchTree;
+
 public class TreeMain {
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+
+        bst.add(30);
+        bst.add(15);
+        bst.add(44);
+        bst.add(33);
+        bst.add(11);
+        bst.add(50);
+        bst.add(100);
+
+        //bst.inOrder();
+        //bst.preOrder();
+        //bst.postOrder();
+        bst.descOrder();
+        System.out.println(bst.search(50));
+        System.out.println(bst.search(55));
+        System.out.println(bst.minimum());
+        System.out.println(bst.maximum());
+    }
 }
--- src/trees/bst/BinarySearchTree.java
+++ src/trees/bst/BinarySearchTree.java
@@ -17,8 +17,126 @@ public class BinarySearchTree<T extends Comparable<T>> {
         if (isEmpty()) {
             root = newNode;
         } else {
-            Node<T> temp = null;
+            addHelper(root, newNode);
         }
+        size++;
+    }
+
+    private void addHelper(Node<T> current, Node<T> newNode) {
+        if (newNode.getKey().compareTo(current.getKey()) < 0) {
+            if (current.getLeft() == null) {
+                current.setLeft(newNode);
+            } else {
+                addHelper(current.getLeft(), newNode);
+            }
+        } else {
+            if (current.getRight() == null) {
+                current.setRight(newNode);
+            } else addHelper(current.getRight(), newNode);
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void inOrder() {
+        inOrder(this.root);
+    }
+
+    private void inOrder(Node<T> root) {
+        if (root == null)
+            return;
+
+        inOrder(root.getLeft());
+        System.out.println(root.getKey());
+        inOrder(root.getRight());
+    }
+
+    public void preOrder() {
+        preOrder(this.root);
+    }
+
+    private void preOrder(Node<T> root) {
+        if (root == null)
+            return;
+
+        System.out.println(root.getKey());
+        preOrder(root.getLeft());
+        preOrder(root.getRight());
+    }
+
+    public void postOrder() {
+        postOrder(this.root);
+    }
+
+    private void postOrder(Node<T> root) {
+        if (root == null)
+            return;
+
+        postOrder(root.getLeft());
+        postOrder(root.getRight());
+        System.out.println(root.getKey());
+    }
+
+    public void descOrder() {
+        descOrder(this.root);
+    }
+
+    private void descOrder(Node<T> root) {
+        if (root == null)
+            return;
+
+        descOrder(root.getRight());
+        System.out.println(root.getKey());
+        descOrder(root.getLeft());
+    }
+
+    public boolean search(T key) {
+        return search(this.root, key);
+    }
+
+    private boolean search(Node<T> root, T key) {
+        if (root == null) return false;
+
+        if (root.getKey().compareTo(key) == 0) return true;
+
+        if (key.compareTo(root.getKey()) < 0)
+            return search(root.getLeft(), key);
+        else
+            return search(root.getRight(), key);
+    }
+
+    public T minimum() {
+        if (isEmpty()) return null;
+        return minimum(this.root);
+    }
+
+    private T minimum(Node<T> root) {
+        Node<T> temp = root;
+        while (temp.getLeft() != null) {
+            temp = temp.getLeft();
+        }
+        return temp.getKey();
+    }
+
+    public T maximum() {
+        if (isEmpty()) return null;
+        return maximum(this.root);
+    }
+
+    private T maximum(Node<T> root) {
+        while (root.getRight() != null) {
+            root = root.getRight();
+        }
+        return root.getKey();
+    }
+
+    public T successor() {
+        if (isEmpty()) return null;
+
+       return minimum(this.root.getRight());
+       // return maximum(this.root.getLeft());
     }
 
 }
