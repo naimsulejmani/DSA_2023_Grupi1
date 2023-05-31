1b205d2ed68d66c5aa176f15913f2fb83a0674ee
Naim Sulejmani <naim.sulejmani@radix.com>
2023-05-31 19:03:58 +0200

    DFS & BFS

--- src/graphs/Graphable.java
+++ src/graphs/Graphable.java
@@ -1,5 +1,9 @@
 package graphs;
 
+import queues.Queue;
+import queues.QueueOverflowException;
+import queues.QueueUnderflowException;
+
 import java.util.List;
 
 public interface Graphable {
@@ -8,4 +12,52 @@ public interface Graphable {
     List<Integer> getAdjacentVertices(int vertex);
 
     void print();
+
+    public static void depthFirstTraversal(Graphable graph, int[] visited, int currentVertex) {
+        if (visited[currentVertex] == 1) return;
+        visited[currentVertex] = 1;
+        List<Integer> list = graph.getAdjacentVertices(currentVertex);
+        for (int vertex : list) {
+            depthFirstTraversal(graph, visited, vertex);
+        }
+        System.out.println(currentVertex + " -> ");
+    }
+
+    public static void depthFirstTraversal(Graphable graphable, int[] visited) {
+        for (int i = 0; i < visited.length; i++) {
+            depthFirstTraversal(graphable, visited, i);
+        }
+    }
+
+    public static void breadthFirstTraversal(Graphable graph, int[] visited, int currentVertex)
+            throws QueueOverflowException, QueueUnderflowException {
+        Queue<Integer> queue = new Queue<>();
+        queue.enqueue(currentVertex);
+        while (!queue.isEmpty()) {
+            int vertex = queue.dequeue();
+            if (visited[vertex] == 1) continue;
+            System.out.print(vertex + " -> ");
+            visited[vertex] = 1;
+            List<Integer> list = graph.getAdjacentVertices(vertex);
+            for (int v : list) {
+                if (visited[v] != 1)
+                    queue.enqueue(v);
+            }
+        }
+    }
+
+    public static void breadthFirstTraversal(Graphable graphable, int[] visited) throws QueueUnderflowException, QueueOverflowException {
+        for (int i = 0; i < visited.length; i++) {
+            breadthFirstTraversal(graphable, visited, i);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
--- src/graphs/TestAdjacencyMatrix.java
+++ src/graphs/TestAdjacencyMatrix.java
@@ -1,16 +1,19 @@
 package graphs;
 
+import queues.QueueOverflowException;
+import queues.QueueUnderflowException;
+
 public class TestAdjacencyMatrix {
-    public static void main(String[] args) {
-        Graphable amg = new AdjacencyListGraph(GraphType.DIRECTED, 5);
+    public static void main(String[] args) throws QueueUnderflowException, QueueOverflowException {
+        Graphable amg = new AdjacencyListGraph(GraphType.UNDIRECTED, 5);
         amg.addEdge(0, 1);
         amg.addEdge(0, 2);
         amg.addEdge(1, 0);
         amg.addEdge(2, 1);
         amg.addEdge(3, 2);
         amg.addEdge(3, 4);
-        amg.addEdge(4,1);
-        amg.addEdge(4,3);
+        amg.addEdge(4, 1);
+        amg.addEdge(4, 3);
 
         amg.print();
 
@@ -20,5 +23,12 @@ public class TestAdjacencyMatrix {
         for (int kojshi : kojshite) {
             System.out.printf("%5d", kojshi);
         }
+
+        System.out.println("\n DFS: ");
+        int[] visited = new int[5];
+        // Graphable.depthFirstTraversal(amg, visited, 4);
+        // Graphable.depthFirstTraversal(amg, visited);
+        Graphable.breadthFirstTraversal(amg, visited, 0);
+
     }
 }
